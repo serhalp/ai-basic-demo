fa93ee218fb94bd768e35b3799cf601823488e06
kathmbeck <katherine.murphy.beck@gmail.com>
2025-08-28 09:20:59 -0400

    add anthropic

--- netlify/functions/openai-no-user-env.ts
+++ netlify/functions/openai-no-user-env.ts
@@ -1,41 +1,24 @@
 import OpenAI from "openai";
 
-export default async function (req: Request): Promise<Response> {
-  process.env["DEBUG"] = "openai-agents:*"
+export default async function (req: Request) {
   console.debug(process.env);
 
-  if (!process.env["OPENAI_API_KEY"]) {
-    return Response.json(
-      { error: "OPENAI_API_KEY is not set" },
-      { status: 500 },
-    );
-  }
 
-  const client = new OpenAI({
-    timeout: 10000
+  const client = new OpenAI({});
+
+  const input = (await req.text()) || "Spit any hot take of your choice";
+
+  const response = await client.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [   
+      {
+        role: "user",
+        content: input,
+      },
+    ],
   });
 
-  console.log(` Making OpenAI request`, {
-    model: "gpt-4o",
-    apiKey: client.apiKey,
-    baseURL: client.baseURL
-  });
-
-  // // const input = (await req.text()) || "Spit any hot take of your choice";
-  // // const response = await client.chat.completions.create({
-  // //   model : "gpt-4o-mini",
-  // //   messages : [ {role : "user", content : "Respond with `Hello world!`"} ],
-  // // });
-
-  // return Response.json({ answer: response.choices[0].message.content });
-
-  const list = await client.models.list();
-  console.log("Available Open AI models:"); 
-  for await (const model of list) {
-    console.log(model.id)
-  }
-
-  return Response.json({ answer: "all good" });
+  return Response.json({ answer: response.choices[0].message.content });
 }
 
 export const config = {
